c1b000b67e067206ee2d5c10ec08f010a36a351b
stambolsky <Borman010884@yandex.ru>
2018-09-18 00:28:37 +0300

    Добавление драйверов для браузеров

--- src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -1,11 +1,18 @@
 package ru.stqa.training.selenium;
 
+import net.bytebuddy.asm.Advice;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
@@ -17,14 +24,19 @@ public class MyFirstTest {
 
     @Before
     public void start() {
-        driver = new ChromeDriver();
+        //driver = new ChromeDriver();
+        //driver = new FirefoxDriver();
+        //driver = new OperaDriver();
+        //driver = new InternetExplorerDriver();
+        //driver = new EdgeDriver();
+        driver = new SafariDriver();
         wait = new WebDriverWait(driver, 10);
     }
 
     @Test
     public void myFirstTest(){
         driver.get("http://www.google.com");
-        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.name("q")).sendKeys("webdriver", Keys.ENTER);
         driver.findElement(By.name("btnK")).click();
         //wait.until(titleIs("webdriver - Поиск в Google"));
     }
